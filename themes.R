a8126a1e0ea8f24892af2661204d119693368bfe
Bernard Surial <surial.bernard@gmail.com>
2021-06-02 16:23:13 +0200

    font to Arial Narrow so it's compatible with work

--- themes.R
+++ themes.R
@@ -1,7 +1,7 @@
 # Custom theme for flextable
 f_theme_surial <- function(ftab) {
   ftab %>% 
-    font(fontname = "Roboto Condensed", part = "all") %>% 
+    font(fontname = "Arial Narrow", part = "all") %>% 
     fontsize(size = 9, part = "body") %>% 
     fontsize(size = 8, part = "footer") %>% 
     align(j = 2, align = "right", part = "all") %>% 
