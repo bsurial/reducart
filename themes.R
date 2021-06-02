4a87f8f22b0098086a888d7cdc00ff89995fd870
Bernard Surial <surial.bernard@gmail.com>
2021-06-02 11:32:28 +0200

    add custom flextable

---
+++ themes.R
@@ -0,0 +1,12 @@
+# Custom theme for flextable
+f_theme_surial <- function(ftab) {
+  ftab %>% 
+    font(fontname = "Roboto Condensed", part = "all") %>% 
+    fontsize(size = 8, part = "body") %>% 
+    fontsize(size = 7, part = "footer") %>% 
+    align(j = 2, align = "right", part = "all") %>% 
+    height(height = 0, part = "body") %>% 
+    line_spacing(space = 0.5) %>% 
+    line_spacing(space = 0.75, part = "footer") %>% 
+    autofit()
+}
